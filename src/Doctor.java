29d302763ca25419b99839759c19d60fad78682b
Shova Yakami <shovayakamionline@gmail.com>
2024-04-27 10:42:22 -0400

    GLAB - 303.9.2 - Basic Inheritance Example

---
+++ src/Doctor.java
@@ -0,0 +1,8 @@
+public class Doctor {
+    String doctorName;
+    String department;
+
+    public void doctorDetails(){
+        System.out.println("Doctor Details.....");
+    }
+}
---
+++ src/Hospital.java
@@ -0,0 +1,8 @@
+public class Hospital {
+    public static void main(String args[]) {
+
+        Surgeon s = new Surgeon();
+        s.doctorDetails();;
+        s.surgeonDetails();
+    }
+}
