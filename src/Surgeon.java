084de93a16f30432706def9e41a4fb6fcf700593
Shova Yakami <shovayakamionline@gmail.com>
2024-04-27 10:47:31 -0400

    GLAB - 303.9.2 - Basic Inheritance Example

--- src/Surgeon.java
+++ src/Surgeon.java
@@ -1,6 +1,6 @@
 public class Surgeon extends Doctor {
     void surgeonDetails() {
-        System.out.println("Surgen Detail.....");
+        System.out.println("Surgeon Detail.....");
         System.out.println(department = "Cardio");
     }
 }
